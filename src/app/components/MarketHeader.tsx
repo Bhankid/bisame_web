2fb68279a00035a08652b077dcf3bb9f6a4af989
Bhankid <alfredfianyo50@gmail.com>
2025-02-06 10:45:30 +0000

    Initial commit from Create Next App

---
+++ src/app/components/MarketHeader.tsx
@@ -0,0 +1,18 @@
+const MarketHeader = () => {
+  return (
+    <div className="bg-black text-white text-sm py-2 px-4 flex justify-between items-center">
+      <div>
+        Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%
+        <a href="#" className="underline ml-2">
+          ShopNow
+        </a>
+      </div>
+      <div className="flex items-center">
+        <span>English</span>
+        <i className="fas fa-chevron-down ml-1"></i>
+      </div>
+    </div>
+  );
+};
+
+export default MarketHeader;
